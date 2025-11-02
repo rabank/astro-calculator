75eb2d011ab279f224f5db840ba4c7cb2ef1f683
rabank <rabank@abv.bg>
2025-11-02 14:51:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 import swisseph as swe
 import math
@@ -86,4 +87,5 @@ def home():
     return "Astro Calculator API is running"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
+
