968607a3e85d6201e9b6b8d4f0a38f9e2267af8e
rabank <rabank@abv.bg>
2026-04-04 18:12:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -794,12 +794,8 @@ def calculate():
 
         # --- Лагна (Ascendant) ---
         # --- DG / JH координати ---
-        if calc_type == "devaguru":
-            lat_use = round(lat, 4)
-            lon_use = round(lon, 1)
-        else:
-            lat_use = lat
-            lon_use = lon
+        lat_use = lat
+        lon_use = lon
 
         # --- Лагна (Ascendant) ---
         ayan_off = NK_AYAN_OFFSET_DG if calc_type == "devaguru" else NK_AYAN_OFFSET_JH
